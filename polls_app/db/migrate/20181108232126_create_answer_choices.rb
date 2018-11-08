7a27a3e9447abb277f7126e298308587864017f6
jlshaw117 <jlshaw117@gmail.com>
2018-11-08 15:33:55 -0800

    Add migrations

--- polls_app/db/migrate/20181108232126_create_answer_choices.rb
+++ polls_app/db/migrate/20181108232126_create_answer_choices.rb
@@ -1,7 +1,8 @@
 class CreateAnswerChoices < ActiveRecord::Migration[5.1]
   def change
     create_table :answer_choices do |t|
-
+      t.integer :question_id, null: false
+      t.text :option, null: false
       t.timestamps
     end
   end
